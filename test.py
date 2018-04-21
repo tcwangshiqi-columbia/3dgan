f395638662bf770770c4505d6998cf655ed13d62
tcwangshiqi-columbia <tcwangshiqi@cs.columbia.edu>
2018-04-21 12:23:57 -0400

    clean up

--- test.py
+++ test.py
@@ -2,9 +2,15 @@ import numpy as np
 import scipy.io as io
 from scipy.io import loadmat
 
+np.set_printoptions(threshold=np.nan)
+
 x = np.random.rand(1,64,64,64)
 
 io.savemat("test",{"voxels":x})
 
-mats = loadmat("chair_demo.mat")
-io.savemat("new_chair",{"voxels":mats["voxels"][4:5]})
+mats = np.load("biasfree_600")
+print mats.shape
+#mats = loadmat("chair_demo.mat")
+#print mats['voxels'].shape
+mats = np.transpose(mats, (0,4,1,2,3))
+io.savemat("new_chair",{"voxels":mats[0:1]})
