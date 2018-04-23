671064064672efc7375e2d2eef908842452a01f2
tcwangshiqi-columbia <tcwangshiqi@cs.columbia.edu>
2018-04-23 17:03:07 -0400

    new interpolation

--- src/vis.py
+++ src/vis.py
@@ -17,11 +17,14 @@ def plotVoxelVisdom(voxels, visdom, title):
 
 #mats = loadmat("../../../3DShapeNets/volumetric_data/desk/30/train/desk_000000001_5.mat")
 #mats = loadmat("image_chair_desk2.mat")
-mats = np.load("interpolation2")
+mats = np.load("interpolation_new1")
 #print (type(mats['voxels']),mats['voxels'].shape)
 #plotVoxelVisdom(np.squeeze(mats['instance']>0.5), vis, 'chair')
-for i in range(100,120):
+for i in range(0,50,2):
 	plotVoxelVisdom(np.squeeze(mats[i]>0.5), vis, 'image'+str(i))
+
+#plotVoxelVisdom(np.squeeze(mats[0]>0.5), vis, 'image'+str(0))
+#plotVoxelVisdom(np.squeeze(mats[49]>0.5), vis, 'image'+str(49))
 
 
 #v, f = getVFByMarchingCubes(mats['instance'])
